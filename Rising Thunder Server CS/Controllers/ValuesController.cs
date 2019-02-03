841c52f7cecd1735ebd7015ea211100f75dc3a1b
Darryl B. Dixon <piecedigitalstudios@gmail.com>
2019-02-03 16:36:55 -0500

    initial commit. project started. negligible code changes

---
+++ Rising Thunder Server CS/Controllers/ValuesController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Rising_Thunder_Server_CS.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class RyzthnController : ControllerBase
+    {
+        // GET ryzthn
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET ryzthn/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST ryzthn
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT ryzthn/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE ryzthn/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
